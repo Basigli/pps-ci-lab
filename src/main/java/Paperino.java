37eacd8704bfe46b78a1d604bc4c5bc8f1c24e04
Gabribrele <gabrielebasigli@gmail.com>
2025-05-05 11:44:36 +0200

    fixed gradle plugin

--- src/main/java/Paperino.java
+++ src/main/java/Paperino.java
@@ -1,7 +1,5 @@
-package main.java;
-
-public class Paperino {
+class Paperino {
     public static void main(String[] args) {
-        System.out.println("Hello, Paperino!");
+        System.out.println("Hello, Paperino here!");
     }
 }
